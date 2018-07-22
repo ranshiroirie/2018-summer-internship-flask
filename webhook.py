0eb3a92b0ae667f40c9686c5bbb81a68271b1eeb
aqmotegi <satoshi.motegi@asia-quest.jp>
2018-07-22 17:06:47 +0900

    最初のコミット

---
+++ webhook.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_assistant import Assistant, ask, tell
+import logging
+
+logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
+
+app = Flask(__name__)
+assist = Assistant(app, route='/')
+
+@assist.action('greeting')
+def greet_and_start():
+    speech = "Hey! Are you male or female?"
+    return ask(speech)
+
+@assist.action("give-gender")
+def ask_for_color(gender):
+    if gender == 'male':
+        gender_msg = 'Sup bro!'
+    else:
+        gender_msg = 'Haay gurl!'
+
+    speech = gender_msg + ' What is your favorite color?'
+    return ask(speech)
+
+@assist.action('give-color', mapping={'color': 'sys.color'})
+def ask_for_season(color):
+    speech = 'Ok, {} is an okay color I guess'.format(color)
+    return ask(speech)
+
+if __name__ == '__main__':
+    app.run(debug=True)
